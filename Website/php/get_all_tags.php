3885b2f8ca1608cc69fa964db5108bcbd58db9b6
peterbillett <peterbillettsemail@gmail.com>
2017-05-18 15:03:27 +1000

    Removing other version of the duplicate file
    Will be recomitted once git is all clear of the duplicate issues.

--- Website/php/get_all_tags.php
+++
@@ -1,9 +0,0 @@
-<?php
-   	include("config.php");
-
-   	echo "<select>";
-	foreach($db->query('SELECT * FROM tag') as $row) {
-	    echo '<option   value=\"'.$row['tagID'].'">'.$row['name'].'</option>';
-	}
-	echo "</select>";
-?>
